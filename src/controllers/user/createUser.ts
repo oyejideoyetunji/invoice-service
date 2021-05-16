6d5ff81acf65f1d16682f424d575376e9c4bdba1
oluwatobi oyejide <moyejide2016@gmail.com>
2021-05-16 17:16:55 +0100

    added middlewares

--- src/controllers/user/createUser.ts
+++ src/controllers/user/createUser.ts
@@ -5,32 +5,36 @@ import User from "../../models/user";
 
 
 
-
 class CreateUser {
     public async createUser(req: Request, res: Response, next: NextFunction){
-        try{
-            const userData: IUser = req.body;
-            const passwordHash = await bcrypt.hash(userData.password, 10);
 
-            const user = new User({
-                firstName: userData.firstName,
-                lastName: userData.lastName,
-                email: userData.email,
-                passwordHash,
-                invoices: userData.invoices || [],
-            });
+        const userData: IUser = req.body;
+        if(!userData.password || userData.password.length < 6){
+            res.status(400).json({ "Error": "Invalid password format" });
+        }else {
+            try{
+                const passwordHash = await bcrypt.hash(userData.password, 10);
 
-            const newUser = await user.save();
-            if(newUser){
-                res.status(201).json(newUser)
-                console.log("User created successfully");
-            }else {
-                res.status(500).json({
-                    message: "sorry there was an error on the server"
-                })
+                const user = new User({
+                    firstName: userData.firstName,
+                    lastName: userData.lastName,
+                    email: userData.email,
+                    passwordHash,
+                    invoices: userData.invoices || [],
+                });
+
+                const newUser = await user.save();
+                if(newUser){
+                    res.status(201).json(newUser)
+                    console.log("User created successfully");
+                }else {
+                    res.status(500).json({
+                        message: "sorry there was an error on the server"
+                    })
+                }
+            }catch(err){
+                next(err)
             }
-        }catch(err){
-            next(err)
         }
     }
 }
